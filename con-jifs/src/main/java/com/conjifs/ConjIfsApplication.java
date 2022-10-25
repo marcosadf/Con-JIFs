1cc230f1bff12322c03540686b9a7b6257ca70c5
Marcos Dourado <marcos.filho18@gmail.com>
2022-10-25 15:52:05 -0300

    rename the project and create the Fat Jar

--- con-jifs/src/main/java/com/conjifs/ConjIfsApplication.java
+++ con-jifs/src/main/java/com/conjifs/ConjIfsApplication.java
@@ -1,10 +1,12 @@
 package com.conjifs;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class ConjIfsApplication {
 
 	public static void main(String[] args) {
--- con-jifs/src/test/java/com/conjifis/ConjIfsApplicationTests.java
+++ con-jifs/src/test/java/com/conjifis/ConjIfsApplicationTests.java
@@ -3,7 +3,9 @@ package com.conjifis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.conjifs.ConjIfsApplication;
+
+@SpringBootTest(classes = ConjIfsApplication.class)
 class ConjIfsApplicationTests {
 
 	@Test
