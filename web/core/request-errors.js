c9467250c4333f20e85df96a6a71772f20f46f70
Marcos Dourado <marcos.filho18@gmail.com>
2022-12-14 16:36:23 -0300

    Web error fix

---
+++ web/core/request-errors.js
@@ -0,0 +1,20 @@
+function request_errors(jqXHR, textStatus, msg){
+	switch (jqXHR.status){
+		case 404:
+			alert(`ERROR ${jqXHR.status}: ${jqXHR.responseJSON.title}`);
+			break;
+		case 400:
+			alert(`ERROR ${jqXHR.status}: ${jqXHR.responseJSON.title}`);
+			break;			
+		case 401:
+			alert(`ERROR ${jqXHR.status}: ${jqXHR.responseJSON.title}`);
+			break;
+		case 403:
+			alert(`Seu login expirou, logar novamente para continuar acessando!`);
+			window.location.replace('./index.html');
+			break;				
+		case 500:
+			alert(`ERROR ${jqXHR.status}: ${jqXHR.responseJSON.title}`);
+			break;			
+	}
+}
