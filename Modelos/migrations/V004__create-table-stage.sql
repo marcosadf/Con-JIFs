915c8fb0c43336782a6234c70e2a666d91bf91ae
Marcos Dourado <marcos.filho18@gmail.com>
2022-12-23 21:49:12 -0300

    Add documentation

--- Modelos/migrations/V004__create-table-stage.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE stage (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  modality_id BIGINT NOT NULL,
-  name_stage ENUM('GROUP', 'FINAL', 'SEMIFINALS', 'QUARTERFINALS', 'ROUNDOF16') NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_stage_modality1
-    FOREIGN KEY (modality_id)
-    REFERENCES modality (id)
-);
--- Modelos/migrations/V009__create-table-manager.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE manager (
-  id BIGINT NOT NULL,
-  login VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
-);
